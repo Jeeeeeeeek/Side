a66dbf28fd1a67a308cf1a24e9eeacfae2d29326
Jeeeeeeeek <jakeanthonycalica@gmail.com>
2019-09-11 10:47:43 +0800

    opo

--- app/src/main/java/com/example/myapplication/MidtermOne.java
+++ app/src/main/java/com/example/myapplication/MidtermOne.java
@@ -3,12 +3,19 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
 
 public class MidtermOne extends AppCompatActivity {
+
+    TextView midtermOneText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_midterm_one);
+
+        midtermOneText = (TextView) findViewById(R.id.midtermOneText);
+        midtermOneText.setText(Html.fromHtml(getString(R.string.midtermone)));
     }
 }
