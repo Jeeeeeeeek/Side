b09bdce094a6d5873bf23066843d8cabd63c2154
Jeeeeeeeek <jakeanthonycalica@gmail.com>
2019-09-11 09:31:39 +0800

    opo

--- app/src/main/java/com/example/myapplication/MidtermTwo_TryAndCatch.java
+++ app/src/main/java/com/example/myapplication/MidtermTwo_TryAndCatch.java
@@ -2,13 +2,23 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MidtermTwo_TryAndCatch extends AppCompatActivity {
 
+    TextView textView10;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_midterm_two__try_and_catch);
+
+        textView10 = (TextView)findViewById(R.id.textView10);
+        textView10.setText(Html.fromHtml(getString(R.string.midtermtwo)));
+
     }
 }
