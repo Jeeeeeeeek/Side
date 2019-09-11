2204c6fb9577bcfd5e1407f7b6bda6c7650f0f32
ITSO Developer <itsodeveloper@ITSO-Daniels-iMac.local>
2019-09-11 09:14:29 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MidtermTwo_TryAndCatch.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MidtermTwo_TryAndCatch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_midterm_two__try_and_catch);
+    }
+}
